b5cf9e2d5824d3ef715ac3f63a5b35cfb20d787b
Fujikawa K <a1934kf@aiit.ac.jp>
2019-06-04 21:49:14 +0900

    initial commit

---
+++ 19745134_tester_kadai3.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# Place arg1, arg2 and correct answer as testnum array.
+testnum=(
+88 33 11 
+132 276 12 
+200 500 100
+6101 18743 1
+204476 154411 527
+9223372036854775807 8970676912557384689 126347562148695559 
+9223372036854775783 3 1
+9223372036854775783 9223372036854775643 1
+9223371873002223329 9223371800114211497 3037000493
+neko saru 0
+1.23 2 0
+2 3.58 0
+鼠 35 0
+38 さる 0
+)
+prog="./19745134_kadai3.sh"
+numsample=$((${#testnum[@]}/3)) 
+
+
+for i in $(seq $numsample)
+do
+        j=$((i-1))
+        arg1=${testnum[$((j*3))]}
+        arg2=${testnum[$((j*3+1))]}
+        correctans=${testnum[$((j*3+2))]}
+	returnans=$($prog "$arg1" "$arg2")
+        if [ "$returnans" -eq "$correctans" ]; then
+                echo "✓ case $i"
+        elif [ $? -gt 0 ] && [ "$correctans" -eq 0 ]; then
+		echo "✓ case $i"
+	else
+		echo"✗ Oops! something is wrong... case $i"
+        fi
+done
+
